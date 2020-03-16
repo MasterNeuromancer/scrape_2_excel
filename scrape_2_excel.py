c6e78579c56d8e789276cfcfba2eca243f7624c8
Neuromancer93 <itrejonumber1@gmail.com>
2020-03-15 17:26:57 -0700

    feat: initial exploration of bs4 api + setup

--- scrape_2_excel.py
+++ scrape_2_excel.py
@@ -1,1 +1,16 @@
-print('Started python scrape to excel project!!!')
+import requests
+from bs4 import BeautifulSoup
+
+result = requests.get('https://www.whitehouse.gov/briefings-statements/')
+
+src = result.content
+
+soup = BeautifulSoup(src, features="lxml")
+
+urls = []
+
+for h2_tag in soup.find_all('h2'):
+    a_tag = h2_tag.find('a')
+    urls.append(a_tag.attrs['href'])
+
+print(urls)
